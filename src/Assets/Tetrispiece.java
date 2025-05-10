7ad097d08c1b3e7eaac4647d1944ed4d19d5fc89
tom-sebastian-67 <tomsebastian67@gmail.com>
2025-05-10 13:57:19 +0530

    colors added

--- src/Assets/Tetrispiece.java
+++ src/Assets/Tetrispiece.java
@@ -1,24 +1,35 @@
 package Assets;
 
-
 public class Tetrispiece {
-    
-    //class contains static pieces - matrix representation
-    //Also their colors
 
-    public static String[] colors = {"#16C79A", "#E94560"}; 
-    public static int[][][] pieces = {
-        {
-            {0, 1, 1}, //L piece 
-            {0, 1, 0},
-            {0, 1, 0}
-        },
-        {
-            {0, 0, 0}, //t piece
-            {0, 1, 0},
-            {1, 1, 1}
-        }
+    // class contains static pieces - matrix representation
+    // Also their colors
+
+    public static String[] colors = { "#00FFFF",
+            "#0000FF",
+            "#FFA500",
+            "#FFFF00",
+            "#00FF00",
+            "#800080",
+            "#FF0000"
     };
 
-    //more colors will be added 
+    public static int[][][] pieces = {
+            {
+                    { 0, 1, 1 }, // L piece
+                    { 0, 1, 0 },
+                    { 0, 1, 0 }
+            },
+            {
+                    { 0, 0, 0 }, // t piece
+                    { 0, 1, 0 },
+                    { 1, 1, 1 }
+            },
+            {
+                    { 0, 1, 0 }, { 0, 1, 0 }, { 0, 1, 0 } // I piece
+            },
+
+    };
+
+    // more colors will be added
 }
